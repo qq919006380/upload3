0fa2e488a1d0bd0d4277537ce3f2339cc930f154
wbc <919006380@qq.com>
2023-10-03 14:37:35 +0800

    add

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,10 @@ import './style.css'
 import App from './App.vue'
 import Upload3 from './components/index';
 // 打印环境变量
-console.log()
-
-
-
-
-
+let token = import.meta.env.VITE_TOKEN
+console.log(token)
 let app = createApp(App)
-app.use(Upload3,{
-    token:import.meta.env.VITE_TOKEN
+app.use(Upload3, {
+    token: token
 })
 app.mount('#app');
